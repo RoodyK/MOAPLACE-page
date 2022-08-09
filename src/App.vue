84e794c7cb44cff599fd35e16d2515afc631d413
RadiantK <pps8853@gmail.com>
2022-08-09 23:20:40 +0900

    회원가입 step1 약관동의 페이지 구현

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <nav>
     <RouterLink to="/">Home</RouterLink> |
     <RouterLink to="/users/login">Login</RouterLink> |
-    <!-- <RouterLink to="/users/same">Same</RouterLink> -->
+    <RouterLink to="/users/same">Same</RouterLink>
   </nav>
   <RouterView/>
 </template>
