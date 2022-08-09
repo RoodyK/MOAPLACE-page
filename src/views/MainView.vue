fa0b398bb464834eeb10b2914ca11c4574074ea1
Hye-In Yoon <heain1001@naver.com>
2022-08-10 07:59:35 +0900

    HEADER 레이아웃 완료(트랜지션 추가필요)

---
+++ src/views/MainView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrap">
+    <AppHeader/>
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+
+export default {
+    name: 'MainView',
+    components:{
+        AppHeader
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .wrap{background: url(http://prugioarthall.daewooenc.com/images/main/main_s_img02.jpg); height: 100vh;}
+</style>
