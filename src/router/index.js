06cbb5b3818b1b5acabd03f1b011ac66f1198ba7
DaHee06 <todayda1006@gmail.com>
2022-08-10 12:22:50 +0900

    모아플레이스안내

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MainView from '../views/moaplace/MainView.vue'
 
 const routes = [
   {
@@ -7,6 +8,11 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+  {
+    path: '/moa/moaplace/info',
+    name: 'MainView',
+    component: MainView
+  }
 ]
 
 const router = createRouter({
