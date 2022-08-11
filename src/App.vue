45eb100c4e4d8c322cb08a53b6533374d31ef3ac
RadiantK <pps8853@gmail.com>
2022-08-11 14:31:38 +0900

    페이지 경로 라우터 설정 및 App.vue 링크 설정

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@
     <RouterLink to="/join/same">Same</RouterLink> |
     <RouterLink to="/join/main">Join</RouterLink> |
     <RouterLink to="/join/success">Success</RouterLink> |
+    <RouterLink to="/login/findid">FindId</RouterLink> |
+    <RouterLink to="/login/findpwd">FindPwd</RouterLink> |
+    <RouterLink to="/login/newpassword">SetNewPassword</RouterLink> |
   </nav>
   <RouterView/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import LoginView from '@/views/login/LoginView.vue'
 import SameView from '../views/join/OfTheSameView.vue'
 import JoinMainView from '../views/join/JoinMainView.vue'
 import JoinSuccessView from '@/views/join/JoinSuccessView.vue'
+import FindIdView from '@/views/login/FindIdView.vue'
+import FindPwdView from '@/views/login/FindPwdView.vue'
+import SetNewPasswordView from '@/views/login/SetNewPasswordView.vue'
 
 const routes = [
   {
@@ -30,6 +33,21 @@ const routes = [
     path: '/join/success',
     name: 'joinSuccess',
     component: JoinSuccessView
+  },
+  {
+    path: '/login/findId',
+    name: 'findId',
+    component: FindIdView
+  },
+  {
+    path: '/login/findPwd',
+    name: 'findPwd',
+    component: FindPwdView
+  },
+  {
+    path: '/login/newpassword',
+    name: 'setNewPassword',
+    component: SetNewPasswordView
   }
 ]
 
