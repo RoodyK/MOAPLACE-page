89e381ab79a997feaac2a10a911b697f91425b4c
walnutB <101103326+walnutB@users.noreply.github.com>
2022-08-11 15:56:44 +0900

    일정달력 완성

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
-    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/">Home</RouterLink> |
+    <RouterLink to="/moa/calendar">월간 일정</RouterLink>
   </nav>
   <RouterView/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
   },
+  {
+    path:'/moa/calendar',
+    name:'ShowCalendar',
+    component: ()=> import('@/views/calendar/Calendar.vue')
+  }
 ]
 
 const router = createRouter({
