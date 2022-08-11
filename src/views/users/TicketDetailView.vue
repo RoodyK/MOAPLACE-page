0b495dd271cd24b4e0ff3be1ff9dbba24a299764
colmaa <neoncoalmine@gmail.com>
2022-08-11 19:32:22 +0900

    예매상세내역 페이지 구현

---
+++ src/views/users/TicketDetailView.vue
@@ -0,0 +1,344 @@
+<template>
+  <div id="wrap">
+    <div id="box" class="black">
+      <!-- 사이드 메뉴 -->
+      <div class="rounded left">
+        <div class="top fs-6">
+          <span class="fs-4 brown">사용자</span>님, 안녕하세요.
+        </div>
+        <div class="mid fs-6 fw-bold">
+          <p>
+            현재 보유 적립금
+          </p>
+          <p class="text-end">
+            <span class="fs-3 text-end">5,000</span>원
+          </p>
+        </div>
+        <div class="bot">
+          <ul class="nav flex-column fs-6">
+            <li class="nav-item">
+              <a class="nav-link" href="#">마이페이지</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">내 관심 공연</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active fw-bold" aria-current="page" href="#">예매내역</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">대관내역</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">관람 후기</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">1:1 문의</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">내 정보 수정</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">로그아웃</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">회원 탈퇴</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- 내역 -->
+      <div class="rounded right">
+        <div>
+          <div class="title">
+            <span class="fs-5 fw-bold">예매내역</span>
+          </div>
+          <div>
+            <table class="table table-borderless myborder ticket-table">
+              <thead>
+                <tr class="text-center">
+                  <th class="col end" colspan="2">Title</th>
+                </tr>
+              </thead>
+              <tbody class="fs-7">
+                <tr>
+                  <th class="col-md-2 text-center descth">예매자</th>
+                  <td class="desctd">고양이</td>
+                </tr>
+                <tr>
+                  <th class="col-md-2 text-center descth">예매번호</th>
+                  <td class="desctd">220807-0000-0001</td>
+                </tr>
+                <tr>
+                  <th class="col-md-2 text-center descth">공연일</th>
+                  <td class="desctd">2022.08.09</td>
+                </tr>
+                <tr>
+                  <th class="col-md-2 text-center descth">회차</th>
+                  <td class="desctd">1회차 14:00</td>
+                </tr>
+                <tr>
+                  <th class="col-md-2 text-center descth">좌석</th>
+                  <td class="desctd">R석 A01, S석 B01</td>
+                </tr>
+                <tr>
+                  <th class="col-md-2 text-center descth">장소</th>
+                  <td class="desctd">공연장1</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div>
+          <div class="title">
+            <span class="fs-5 fw-bold">결제내역</span>
+          </div>
+          <div>
+            <table class="table table-borderless myborder ticket-table">
+              <tbody class="fs-7">
+                <tr>
+                  <th class="col-md-2 text-center descth">결제일</th>
+                  <td class="desctd">2022.08.01</td>
+                </tr>
+                <tr>
+                  <th class="col-md-2 text-center descth">결제금액</th>
+                  <td class="desctd">250,000원</td>
+                </tr>
+                <tr>
+                  <th class="col-md-2 text-center descth">결제수단</th>
+                  <td class="desctd">신용카드</td>
+                </tr>
+                <tr>
+                  <th class="col-md-2 text-center descth">결제상태</th>
+                  <td class="desctd">입금완료</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="text-center btnmargin">
+          <button type="button" class="btn btn-outline-secondary fs-6 fw-bold mybtn">예매취소</button>
+          <button type="button" class="btn btn-outline-secondary fs-6 fw-bold mybtn2">이전으로</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TicketDetailView',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../scss/common.scss';
+
+#wrap {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  background-color: rgb(249, 249, 249);
+  a {
+    color: $black;
+    text-decoration:none;
+  }
+  .fs-7 {
+    font-size: 0.8em;
+  }
+  .black {
+    color: $black;
+  }
+  .brown {
+    color: $brown;
+  }
+  #box {
+    display: flex;
+    justify-content: center;
+    width: $width;
+    margin: 50px;
+    .left {
+      width: 300px;
+      height: 600px;
+      background-color: white;
+      border: 2px solid #dbe2e8;
+      margin-right: 30px;
+      padding: 30px 20px;
+      .top {
+        margin-bottom: 10px;
+      }
+      .mid {
+        background-color: whitesmoke;
+        border: 2px solid #dbe2e8;
+        padding: 15px;
+        p {
+          margin: 0;
+        }
+        span {
+          color: #D67747;
+        }
+      }
+      .bot {
+        margin: 10px 0px;
+        ul li {
+          border-bottom: 1px solid #CCCCCC;
+          a {
+            color: $black;
+          }
+          a:hover {
+            color: $brown;
+            opacity: 50%;
+          }
+          .active {
+              color: $brown;
+          }
+        }
+      }
+    }
+    .right {
+      width: 100%;
+      height: 100%;
+      background-color: white;
+      border: 2px solid #dbe2e8;
+      padding: 30px;
+      .btnmargin {
+        margin-top: 30px;
+      }
+      .mybtn {
+        color: $brown;
+        border-color: $brown;
+        background-color: white;
+        padding: 5px 50px;
+        &:hover {
+          color: white;
+          background: $brown;
+        }
+      }
+      .mybtn2 {
+        color: white;
+        border-color: $brown;
+        background-color: $brown;
+        padding: 5px 50px;
+        margin-left: 20px;
+        &:hover {
+          color: $brown;
+          background: white;
+          border-color: $brown;
+        }
+      }
+      .title {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 10px;
+      }
+      .datenav {
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        height: 70px;
+        margin: 30px 0px;
+        .mydate {
+          padding: 5px;
+        }
+        .datebtn {
+          padding: 5px 20px;
+          border: 1px solid gray;
+          background-color: white;
+        }
+        .select {
+          background-color: $brown;
+          a {
+            color: white;
+          }
+        }
+        .resultbtn {
+          margin-left: 10px;
+          font-size: 1em;
+        }
+      }
+      .table {
+        th.col {
+          background-color: $brown;
+          color: white;
+        }
+        tbody {
+          td:hover {
+            background-color: rgb(249, 249, 249);
+          }
+        }
+        td {
+          background-color: white;
+          .info {
+            display: flex;
+            margin: 20px 20px;
+            .img1 {
+              background-color: gray;
+              width: 110px;
+              height: 150px;
+              margin-right: 20px;
+            }
+            .img2 {
+              background-color: gray;
+              width: 55px;
+              height: 75px;
+              margin-right: 20px;
+            }
+            .txt {
+              p {
+                margin: 0px;
+              }
+              table td,th {
+                border: 0px;
+                background-color: transparent;
+              }
+            }
+          }
+        }
+      }
+      .myborder {
+        th{
+          border-right: 1px solid #eee;
+        }
+        td{
+          border-right: 1px solid #dbe2e8;
+          border-bottom: 1px solid #dbe2e8;
+        }
+        .descth {
+          border: 1px solid #dbe2e8;
+          background-color: rgb(249, 249, 249);
+        }
+        .desctd {
+          border: 1px solid #dbe2e8;
+        }
+        .end{
+          border-right: 0px;
+        }
+      }
+      .ticket-table {
+        vertical-align : middle;
+      }
+      .datenav {
+        background-color: rgb(249, 249, 249);
+      }
+    }
+  }
+  #mypaging {
+    display: flex;
+    justify-content: center;
+    .select {
+      font-weight: bold;
+    }
+    li {
+      a,span,a:hover,span:hover,a:focus,span:focus,a:active {
+        background: transparent;
+        border: none;
+        box-shadow: none;
+      }
+      a:hover {
+        color: $brown;
+        opacity: 50%;
+      }
+    }
+  }
+}
+</style>
