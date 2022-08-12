6f89cb5bc3b7028a63034cbd0e06ad5f6afa35f2
Hye-In Yoon <heain1001@naver.com>
2022-08-12 21:56:16 +0900

    대관정보 페이지 완료

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <RouterLink to="/">Home</RouterLink> | 
-    <RouterLink to="/moa/rental/insert">대관신청</RouterLink>
+    <RouterLink to="/moa/rental/insert">대관신청</RouterLink> | 
+    <RouterLink to="/moa/rental">대관정보</RouterLink>
     <RouterView/>
   </nav>
 </template>
