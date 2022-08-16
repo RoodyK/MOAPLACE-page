e01672d8e6f8e8f9516d1e64202fc3ad7468ff36
RadiantK <pps8853@gmail.com>
2022-08-16 11:12:54 +0900

    main 병합을 위한 기본파일 초기화

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <nav>
-    <RouterLink to="/">Home</RouterLink> |
-    <RouterLink to="/moa/calendar">월간 일정</RouterLink> |
-    <RouterLink to="/moa/showlist">ShowList</RouterLink> |
-    <RouterLink to="/moa/showdetail">ShowDetail</RouterLink> |
-    <RouterLink to="/moa/showreview">ShowReview</RouterLink> |
-    <RouterLink to="/moa/showrefund">ShowRefund</RouterLink>
+    <RouterLink to="/">Home</RouterLink>
   </nav>
   <RouterView/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
   },
-  {
-    path:'/moa/calendar',
-    name:'ShowCalendar',
-    component: ()=> import('@/views/calendar/Calendar.vue')
-  },
-  {
-    path: '/moa/showlist',
-    name: 'showlist',
-    component: () => import('../views/show/ShowListView.vue')
-  },
-  {
-    path: '/moa/showdetail',
-    name: 'showdetail',
-    component: () => import('../views/show/ShowDetailView.vue')
-  },
-  {
-    path: '/moa/showreview',
-    name: 'showreview',
-    component: () => import('../views/show/ShowReviewView.vue')
-  },
-  {
-    path: '/moa/showrefund',
-    name: 'showrefund',
-    component: () => import('../views/show/ShowRefundView.vue')
-  }
 ]
 
 const router = createRouter({
