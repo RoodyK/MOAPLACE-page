62a8d32569850a22729eefd4a77b69581a687562
RadiantK <pps8853@gmail.com>
2022-08-16 11:09:55 +0900

    MAIN 병합을 위한 MOA-3 초기화

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
-  <div>
-    <nav>
-      <RouterLink to="/">Home</RouterLink> |
-      <RouterLink to="/moa/moaplace/info">MOAPLACE 소개</RouterLink> |
-      <RouterLink to="/moa/moaplace/guide">MOAPLACE 홀 안내</RouterLink> |
-      <RouterLink to="/moa/moaplace/navi">MOAPLACE 오시는 길 안내</RouterLink> |
-      <RouterLink to="/moa/moaplace/news">MOAPLACE 새소식</RouterLink> |
-      <RouterLink to="/moa/moaplace/news/detail"
-        >MOAPLACE 새소식 상세페이지</RouterLink
-      >
-      |
-      <RouterLink to="/moa/moaplace/news/update"
-        >MOAPLACE 새소식 수정페이지</RouterLink
-      >
-    </nav>
-
-    <RouterView />
-  </div>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+  </nav>
+  <RouterView/>
 </template>
 
 <style>
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import moaplaceMainView from '../views/moaplace/MainView.vue'
-import moaplaceInfoView from '../views/moaplace/InfoView.vue'
-import moaplaceInfoView2 from '../views/moaplace/InfoView2.vue'
-import moaplaceInfoView3 from '../views/moaplace/InfoView3.vue'
-import moaplaceNaviView from '../views/moaplace/NaviView.vue'
-import moaplaceMoaNews from '../views/moaplace/MoaNews.vue'
-import moaplaceMoaDetail from '../views/moaplace/MoaDetail.vue'
-import moaplaceMoaUpdate from '../views/moaplace/MoaUpdate.vue'
 
 const routes = [
   {
@@ -15,47 +7,6 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-  {
-    path: '/moa/moaplace/info',
-    name: 'moaplaceMainView',
-    component: moaplaceMainView
-  },
-  {
-    path: '/moa/moaplace/guide',
-    name: 'moaplaceInfoView',
-    component: moaplaceInfoView
-  },
-  {
-    path: '/moa/moaplace/guide2',
-    name: 'moaplaceInfoView2',
-    component: moaplaceInfoView2
-  },
-  {
-    path: '/moa/moaplace/guide3',
-    name: 'moaplaceInfoView3',
-    component: moaplaceInfoView3
-  },
-  {
-    path: '/moa/moaplace/navi',
-    name: 'moaplaceNaviView',
-    component: moaplaceNaviView
-  },
-  {
-    path: '/moa/moaplace/news',
-    name: 'moaplaceMoaNews',
-    component: moaplaceMoaNews
-  },
-  {
-    path: '/moa/moaplace/news/detail',
-    name: 'moaplaceMoaDetail',
-    component: moaplaceMoaDetail
-  },
-  {
-    path: '/moa/moaplace/news/update',
-    name: 'moaplaceMoaUpdate',
-    component: moaplaceMoaUpdate
-  }
-
 ]
 
 const router = createRouter({
