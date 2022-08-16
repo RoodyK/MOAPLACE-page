2a968172044776e2c317722f4304a77a9baed9c8
RadiantK <pps8853@gmail.com>
2022-08-16 11:05:34 +0900

    메인 병합을 위한 초기화

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <nav>
-    <RouterLink to="/">Home</RouterLink> |
-    <RouterLink to="/users/login">Login</RouterLink> |
-    <RouterLink to="/join/same">Same</RouterLink> |
-    <RouterLink to="/join/main">Join</RouterLink> |
-    <RouterLink to="/join/success">Success</RouterLink> |
-    <RouterLink to="/login/findid">FindId</RouterLink> |
-    <RouterLink to="/login/findpwd">FindPwd</RouterLink> |
-    <RouterLink to="/login/newpassword">SetNewPassword</RouterLink> |
+    <RouterLink to="/">Home</RouterLink>
   </nav>
   <RouterView/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LoginView from '@/views/login/LoginView.vue'
-import SameView from '../views/join/OfTheSameView.vue'
-import JoinMainView from '../views/join/JoinMainView.vue'
-import JoinSuccessView from '@/views/join/JoinSuccessView.vue'
-import FindIdView from '@/views/login/FindIdView.vue'
-import FindPwdView from '@/views/login/FindPwdView.vue'
-import SetNewPasswordView from '@/views/login/SetNewPasswordView.vue'
 
 const routes = [
   {
@@ -14,41 +7,6 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-  {
-    path: '/users/login',
-    name: 'login',
-    component: LoginView
-  },
-  {
-    path: '/join/same',
-    name: 'same',
-    component: SameView
-  },
-  {
-    path: '/join/main',
-    name: 'join',
-    component: JoinMainView
-  },
-  {
-    path: '/join/success',
-    name: 'joinSuccess',
-    component: JoinSuccessView
-  },
-  {
-    path: '/login/findId',
-    name: 'findId',
-    component: FindIdView
-  },
-  {
-    path: '/login/findPwd',
-    name: 'findPwd',
-    component: FindPwdView
-  },
-  {
-    path: '/login/newpassword',
-    name: 'setNewPassword',
-    component: SetNewPasswordView
-  }
 ]
 
 const router = createRouter({
