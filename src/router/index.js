64b423bced36c895feecb8d380d1b8e6b691a344
RadiantK <pps8853@gmail.com>
2022-08-16 11:17:15 +0900

    main 병합을 위한 기본파일 초기화

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import RentalInsertView from '../views/rental/RentalInsertView.vue'
-import RentalInfoView from '../views/rental/RentalInfoView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-    path: '/moa/rental/insert',
-    name: 'rentalInsert',
-    component: RentalInsertView
-  },
-  {
-    path: '/moa/rental',
-    name: 'rentalInfo',
-    component: RentalInfoView
   },
 ]
 
