69fd07531dff596e60dfd06f737b2c13fd5504f5
Seongmin <chr92@naver.com>
2022-08-18 09:37:05 +0900

    예매-할인선택 페이지 구현

--- src/router/index.js
+++ src/router/index.js
@@ -346,7 +346,11 @@ const routes = [
     name: 'adminNewsInsert',
     component: AdminNewsInsert
   },
-
+  {
+    path: '/moaplace.com/booking/cnt',
+    name: 'bookingCnt',
+    component: () => import('@/views/BookingCntView.vue')
+  },
 ]
 
 const router = createRouter({
