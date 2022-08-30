bc6a8dc3e134016725816477667e44ee4aa13b6c
Seongmin <chr92@naver.com>
2022-08-30 17:50:14 +0900

    사용자 FAQ페이지 구현(검색,페이지) 및 1:1문의 답변완료 메일전송 적용, 기타 수정 등

--- src/views/admin/board/FAQList.vue
+++ src/views/admin/board/FAQList.vue
@@ -9,9 +9,9 @@
           <div class="top-1">
             <select v-model="sort_num" @change="searchList()">
               <option value="0"> 전체 </option>
-                <option v-for="sort in sort_list" :key="sort" :value="sort.sort_num">
+              <option v-for="sort in sort_list" :key="sort" :value="sort.sort_num">
                 {{sort.sort_name}} 문의
-                </option>
+              </option>
             </select>
           </div>
 
@@ -22,8 +22,7 @@
               <option value="faq_content">내용</option>
             </select>
             <input type="text" v-model="newKeyword" @keyup.enter="searchList()">
-              <i class="material-icons"  @click="searchList()">search</i>
-
+            <i class="material-icons"  @click="searchList()">search</i>
             <button @click="$router.push({name:'adminFaqInsert'})">FAQ등록</button>
           </div>
         </div>
@@ -96,10 +95,10 @@ export default {
     return {
       sort_list:[],
       list: [],
-      pageNum: 1, // 현재 페이지
       field:'', // 검색필드
       keyword:'', // 검색어
       newKeyword:'', // 검색어 변경
+      pageNum: 1, // 현재 페이지
       startPage:1, // 페이지 시작번호
       endPage:1, // 페이지 마지막번호
       pageCnt:1, // 전체 페이지 개수
@@ -114,58 +113,67 @@ export default {
       this.keyword = this.$route.params.keyword;
       this.field = this.$route.params.field;
     }
-
     console.log(this.pageNum, this.field, this.keyword);
+    
     this.sortList();
     this.faqList(); // 리스트 불러오기
   },
   methods: {
-    async sortList() { // 구분목록 불러오기
-      try {
-        await axios.get('/moaplace.com/board/sort/list').then(function(resp){
-          if(resp.data!=null || resp.data!=''){
-            this.sort_list = resp.data;
-
-          } else {
-            alert('구분목록 로딩에 실패하였습니다.');
-          }
-        }.bind(this));
-
-      } catch (error) {
-        console.log(error);
-      }                
-    },     
+    async sortList() {
+      await axios.get('/moaplace.com/board/sort/list')
+                  .then(resp => {
+                    this.sort_list = resp.data;
+                  })
+                  .catch (error => {
+                    console.log(error);
+                  })
+    },            
     async faqList() {
-      try { 
-        await axios.get("/moaplace.com/admin/faq/list", {params:
-          { pageNum: this.pageNum,
-            field: this.field,
-            keyword: this.keyword,
-            sort_num: this.sort_num }
-          }).then(function(resp){
+      await axios.get("/moaplace.com/admin/faq/list", 
+                    { params:
+                      { pageNum: this.pageNum,
+                        field: this.field,
+                        keyword: this.keyword,
+                        sort_num: this.sort_num }
+                  })
+                  .then(resp => {
+                    this.list = resp.data.list // 리스트
 
-          if(resp.status == 200) {
-            this.pageNum = resp.data.pageNum, // 페이지 번호
-            this.field = resp.data.field, // 검색어
-            this.keyword = resp.data.keyword, // 검색어
-            this.newKeyword = resp.data.keyword, // 검색어 변경
-            this.list = resp.data.list, // 문의글 리스트
-            this.startPage = resp.data.startPage, // 페이지 시작번호
-            this.endPage = resp.data.endPage, // 페이지 마지막번호
-            this.pageCnt= resp.data.pageCnt // 전체 페이지 개수
-            this.sort_num = resp.data.sort_num // 필터용 구분번호
+                    if(resp.data.field=='all'){
+                      this.field = ''
+                    } else {
+                      this.field = resp.data.field // 검색필드
+                    }
 
-          } else {
-            alert('페이지 로딩에 실패하였습니다. 다시 시도해주세요.');
-          }
-        }.bind(this));
-      } catch (error) {
-          console.log(error);
-      }
+                    this.keyword = resp.data.keyword, // 검색어
+                    this.newKeyword = resp.data.keyword, // 검색어 변경
+                    this.sort_num = resp.data.sort_num, // 필터용 구분번호
+                    this.pageNum = resp.data.pageNum, // 페이지 번호
+                    this.startPage = resp.data.startPage // 페이지 시작번호
+
+                    if(resp.data.endPage < 1){
+                      this.endPage = 1,
+                      this.pageCnt = 1
+                    } else {
+                      this.endPage = resp.data.endPage, // 페이지 마지막번호
+                      this.pageCnt= resp.data.pageCnt // 전체 페이지 개수
+                    }
+                  }) 
+                  .catch (error => {
+                    console.log(error);
+                  })
     },
     searchList(){ // 리스트 검색
-      this.pageNum = 1;
+      if(this.field=='' || this.field==null) {
+        alert('검색 구분을 선택하세요.')
+        return;
+      }
+      if(this.newKeyword=='' || this.newKeyword==null){
+        alert('검색어를 입력하세요.')
+        return;
+      } 
       this.keyword = this.newKeyword; // 검색어 변경
+      this.pageNum = 1;
       console.log(this.field, this.keyword, this.sort_num);
 
       this.faqList();
@@ -178,31 +186,27 @@ export default {
     },
     async deleteFaq(rnum, faq_num){ // faq 삭제
       if(confirm('FAQ '+rnum+'번을 삭제하시겠습니까?')){
-        try { 
-          await axios.post("/moaplace.com/admin/faq/delete/"+faq_num)
-                     .then(function(resp){
+        await axios.post("/moaplace.com/admin/faq/delete/"+faq_num)
+                   .then(resp => {
+                      if(resp.data!='fail') {
+                        alert('FAQ가 삭제되었습니다.');
+                        this.faqList();
 
-            if(resp.status == 200) {
-              alert('FAQ가 삭제되었습니다.');
-              this.faqList();
-
-            } else {
-              alert('FAQ 삭제를 실패하였습니다. 다시 시도해주세요.');
-            }
-          }.bind(this));
-
-        } catch (error) {
-          console.log(error);
-        }  
-      } else {
-        return;
-      }
+                      } else {
+                        alert('FAQ 삭제를 실패하였습니다. 다시 시도해주세요.');
+                        return;
+                      }
+                    })
+                   .catch (error => {
+                      console.log(error);
+                    })
+      } else return;
     },
     prevent(e) {
       e.stopPropagation();
     }    
   }
-};
+}
 </script>
 
 <style lang="scss" scoped="scoped">
@@ -342,11 +346,12 @@ nav {
         }
       }
       .empty-list{
-        height: 300px;
+        height: 160px;
         display: flex;
         justify-content: center;
         align-items: center;
         border-bottom: 1px solid rgba($black, 0.1);
+        color: rgba($black, 0.7);
         i{
           margin-right:8px;
         }
