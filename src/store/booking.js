3d74ca6cf3011a82ea15fba5b656ad3a156a2b23
Hye-In Yoon <heain1001@naver.com>
2022-08-31 19:15:04 +0900

    booking,js state 추가

--- src/store/booking.js
+++ src/store/booking.js
@@ -1,7 +1,5 @@
 //booking.js 예매와 관련된 데이터 취급
 
-
-
 export default {
     namespaced: true,
     state(){
@@ -14,6 +12,11 @@ export default {
             schedule_date : "", //양식 - 22.08.16(화)
             round : "", //양식 - 1회차 14:30
             seats : [], //양식 - S석 D09, S석 E09
+            rows: 0,//열번호
+            cols: 0,//행번호
+            priceR: 0, //R석 가격
+            priceS: 0,
+            priceA: 0,
             gradeR : 0, //R석 매수
             gradeS : 0, //S석 매수
             gradeA : 0, //A석 매수
